4d68ccfd0a5c8b80dd5339764af143befe126d89
VerliererK <r03548014@ntu.edu.tw>
2019-08-17 01:54:19 +0800

    add test jpg, font to arial, fix 3d view overflow

--- js/client.js
+++ js/client.js
@@ -17,13 +17,13 @@ function testPic() {
         if (this.readyState == 4 && this.status == 200) {
             $('#mediaFile').val('');
             MyFile = this.response;
-            $('#profile').css('background-image', 'url(' + 'https://cdn.glitch.com/034b2a68-30f8-4128-8780-8962b74387b3%2Ftest.jpg?v=1565545089944' + ')');
+            $('#profile').css('background-image', 'url(' + '/test.jpg' + ')');
             $('#profile label').addClass("hasImage");
             $('#profile .dashes').addClass("hasImage");
             //console.log(this.response, typeof this.response);
         }
     }
-    xhr.open('GET', 'https://cdn.glitch.com/034b2a68-30f8-4128-8780-8962b74387b3%2Ftest.jpg?v=1565545089944');
+    xhr.open('GET', '/test.jpg');
     xhr.responseType = 'blob';
     xhr.send();
 }
